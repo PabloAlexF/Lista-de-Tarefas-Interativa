c80622ed7fcf2a472ef006dc04d7f6e9f404e7ec
Pablo Andrade <Pablolexandrade@gmail.com>
2025-03-06 18:46:13 -0300

    Efeito de menu adicionado em js

---
+++ src/js/configurarMenuBTN.js
@@ -0,0 +1,15 @@
+const btn_Menu = document.querySelector("#menu");
+const menu = document.querySelector("#opcoes");
+let contador = 0;
+
+btn_Menu.addEventListener("click", () => {
+    contador++;
+    menu.style.transition = "2s";
+
+    if (contador > 1) {
+        menu.style.left = "-250px";
+        contador = 0;
+    } else {
+        menu.style.left = "0px";
+    }
+});
